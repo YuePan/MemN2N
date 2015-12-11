2ac54c4f0ed047e22d30a9d676f71c0e2118f42c
Yue Pan <yupan@microsoft.com>
2015-12-11 22:29:10 +0800

    Signed-off-by: Yue Pan <yupan@microsoft.com>

--- run.sh
+++ run.sh
@@ -2,9 +2,10 @@
 
 # tasks="1 2 3 4 5 6 9 10 11 12 13 14 15 16 17 18 20"
 tasks="11 12 13 14 15 16 17 18 20"
+runs="0 1 2 3 4 5 6 7 8 9"
 time for task in $tasks; do
   echo "running task: $task"
-  for run in `jot 10`; do
+  for run in $runs; do
     python -u main.py --task $task > results/${task}_${run}.txt 2>/dev/null &
   done
   wait
